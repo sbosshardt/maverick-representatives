8c532c9224c653b9ae7a128b7c496e2fc727cd48
Samuel Bosshardt <5762452+sbosshardt@users.noreply.github.com>
2024-08-05 16:52:25 -0700

    Modified sample SQL query to return additional data, focus more on more extreme differences of voting vs caucus.

--- SQL queries/Duarte vs caucus.sql
+++ SQL queries/Duarte vs caucus.sql
@@ -1,8 +1,12 @@
 SELECT
   l.legislator_name,
   r.rollcall_num,
+  r.session,
   r.legis_num,
   r.vote_question,
+  r.vote_desc,
+  r.amendment_num,
+  r.amendment_author,
   t.yea,
   t.nay,
   v.vote
@@ -20,4 +24,7 @@ WHERE
 (
   (t.yea > t.nay AND (v.vote = 'No' or v.vote = 'Nay')) OR
   (t.yea < t.nay AND (v.vote = 'Yea' OR v.vote = 'Aye'))
-)
+) AND
+(t.yea < 25 OR t.nay < 25)
+GROUP BY legis_num
+ORDER BY yea DESC
